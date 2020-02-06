bdddede5f5be329bff7c48b1cc26d762af897ba1
NicolasDutour <dutourn99@gmail.com>
2020-02-06 16:17:42 +0100

    Add env variables and backgroudn image plugin

---
+++ src/components/Layout/Hero/Hero.js
@@ -0,0 +1,20 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+const HeroImage = styled.div`
+  width: 100%;
+  height: calc(100vh - 80px);
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Hero = () => {
+  return <div></div>
+}
+
+export default Hero
